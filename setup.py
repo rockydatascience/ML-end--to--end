6b8c98e1ae36091fb88b63fd9ce9c6ffd5bd0cc8
rockydatascience <rockybusinessmail4564@gmail.com>
2024-08-09 13:49:07 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hypen_e_dot = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requiremnets
+    '''
+    
+    requirements = []
+    with open (file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+        
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+            
+    return requirements
+
+setup(
+name="MLproject",
+version="0.0.1",
+author="Rakesh", 
+author_email="rockybusinessmail4564@gmail.com",
+packages=find_packages(),
+install_requires= get_requirements('requirements.txt') 
+)
